cd474f6baf4e4619ecd9d1a36d6366d08701f217
ChuongPT_Desktop <phamtienchuong2000@gmail.com>
2022-10-11 18:21:54 +0700

    steering

--- Assets/Scripts/KartController/Kart.cs
+++ Assets/Scripts/KartController/Kart.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Kart : MonoBehaviour
 {
+    [Header("Suspensions")]
+    [SerializeField] float _tireMass;
     [SerializeField] Transform _suspensions;
 
     [SerializeField] private float _length;
@@ -12,7 +14,17 @@ public class Kart : MonoBehaviour
     [SerializeField] private float _damper;
     [SerializeField] private LayerMask _groundLayerMask;
 
-    private Rigidbody _carRigidbody => GetComponent<Rigidbody>();
+    private Rigidbody _rigidbody => GetComponent<Rigidbody>();
+
+    [Header("Steering")]
+    [SerializeField] float _maxSteeringSpeed;
+    [SerializeField] AnimationCurve _frontTiresGrip;
+    [SerializeField] AnimationCurve _rearTiresGrip;
+
+    [Header("Acceleration")]
+    [SerializeField] float _topSpeed;
+    private float _accelerationInput;
+
 
     private void FixedUpdate()
     {
@@ -22,14 +34,28 @@ public class Kart : MonoBehaviour
 
             RaycastHit hit;
             bool isRayHit = Physics.Raycast(suspension.position, this.transform.up * -1, out hit, _length, _groundLayerMask);
-            if(isRayHit)
+            if (isRayHit)
             {
+                // Suspension force
                 Vector3 springDir = suspension.up;
-                Vector3 springVel = _carRigidbody.GetPointVelocity(suspension.position);
+                Vector3 springVel = _rigidbody.GetPointVelocity(suspension.position);
                 float offet = _length - hit.distance;
                 float velocity = Vector3.Dot(springDir, springVel);
                 float force = (offet * _strength) - (velocity * _damper);
-                _carRigidbody.AddForceAtPosition(springDir * force, suspension.position);
+                _rigidbody.AddForceAtPosition(springDir * force, suspension.position);
+
+                // Steering force
+                Vector3 steerDir = suspension.right;
+                Vector3 tireWoldVel = springVel;
+                float steerVel = Vector3.Dot(steerDir, tireWoldVel);
+                float steerVelNormalized = Mathf.Clamp01(Mathf.Abs(steerVel) / _maxSteeringSpeed);
+                float tireGripFactor = (suspension.GetSiblingIndex() < 2) ? _frontTiresGrip.Evaluate(steerVelNormalized) : _rearTiresGrip.Evaluate(steerVelNormalized);
+                float desiredVelChange = -steerVel * tireGripFactor;
+                float desiredAccel = desiredVelChange / Time.fixedDeltaTime;
+                _rigidbody.AddForceAtPosition(steerDir * _tireMass * desiredAccel, suspension.position);
+
+                // Acceleration / Braking
+
             }
         }
     }
