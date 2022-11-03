d232c6d76811e88a2c6f4acb040d758d79f33cdd
ChuongPT <phamtienchuong2000@gmail.com>
2022-11-03 22:07:57 +0700

    test position system ok

--- Assets/Scripts/Position System/PositionSystem.cs
+++ Assets/Scripts/Position System/PositionSystem.cs
@@ -8,17 +8,39 @@ public class PositionSystem : MonoBehaviour
 {
     public PathCreator pathCreator;
     public Transform kartGroup;
+    public Transform finishLine;
     public List<float> distances = new List<float>();
+
+    private void Awake()
+    {
+        distances.Clear();
+        foreach (Transform child in kartGroup)
+        {
+            distances.Add(0f);
+        }
+    }
 
     [Button]
     public void GetKartsPositions()
     {
         var path = pathCreator.path;
-        distances.Clear();
-        foreach(Transform kartTransform in kartGroup)
+        Debug.Log(path.length);
+        for (int i = 0; i < kartGroup.childCount; i++)
         {
-            float distance = path.GetClosestDistanceAlongPath(kartTransform.position);
-            distances.Add(distance);
+            Vector3 kartPos = kartGroup.GetChild(i).position;
+            float distanceAlongPath = path.GetClosestDistanceAlongPath(kartPos);
+            float finishLineDistanceAlongPath = path.GetClosestDistanceAlongPath(
+                finishLine.position
+            );
+            float distance = distanceAlongPath - finishLineDistanceAlongPath;
+            if (distance < 0)
+                distance = distanceAlongPath + finishLineDistanceAlongPath;
+            distances[i] = distance;
         }
+    }
+
+    private void Update()
+    {
+        // GetKartsPositions();
     }
 }
