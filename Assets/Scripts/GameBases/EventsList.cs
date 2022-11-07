beb8a9ec4f1faa03e65f4324578e3576cce24075
ChuongPT <phamtienchuong2000@gmail.com>
2022-11-07 16:08:16 +0700

    add new event change game state to event list

--- Assets/Scripts/GameBases/EventsList.cs
+++ Assets/Scripts/GameBases/EventsList.cs
@@ -9,11 +9,13 @@ public static class EventsList
 
 public static class GameEvent
 {
-    public const string Empty_Event = "Empty_Event";
+    public const string Empty_Event = "GameEvent.Empty_Event";
 }
 
 public static class EventGameplay
 {
-    public const string Empty_Event = "Empty_Event";
-    public const string Kart_Cross_Finish_Line = "Kart_Cross_Finish_Line";
+    public const string Empty_Event = "EventGameplay.Empty_Event";
+    public const string Kart_Cross_Finish_Line = "EventGameplay.Kart_Cross_Finish_Line";
+    public const string Change_State_Game = "EventGameplay.Change_State_Game";
+
 }
